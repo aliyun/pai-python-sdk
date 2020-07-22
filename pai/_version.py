8d99df03a94ad66223885a4f1000ead62d269a13
liangquan.lq <lq262469@alibaba-inc.com>
2020-07-22 19:55:24 +0800

    set package as developmental releases.

--- pai/_version.py
+++ pai/_version.py
@@ -1,12 +1,5 @@
 from __future__ import absolute_import
 
-version_info = (0, 1, 2, "rc0")
+version_info = (0, 1, 2, "dev0")
 
-_num_index = max(idx if isinstance(v, int) else 0
-                 for idx, v in enumerate(version_info))
-version_suffix = ".".join(version_info[_num_index+1:])
-
-if version_suffix:
-    __version__ = '.'.join(map(str, version_info[:_num_index + 1])) + '.' + version_suffix
-else:
-   __version__ = '.'.join(map(str, version_info[:_num_index + 1]))
+__version__ = '.'.join(map(str, version_info))
