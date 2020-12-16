a169236849fab2a827345110dbafcfc313aa0243
liangquan.lq <lq262469@alibaba-inc.com>
2020-12-16 20:28:08 +0800

    [to #31959924] chore: add shell script for documents publish.
    Signed-off-by: liangquan.lq <lq262469@alibaba-inc.com>

---
+++ tools/publish_doc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#To use the script, you must have ossutil in your system PATH and config the client tool to visit the "pai-sdk" OSS bucket.
+#
+#usage:
+#
+# publish_doc preview              # publish preview documents
+# publish_doc production           # publish production documents
+#
+
+function cd_docs_dir() {
+  docs_dir="$(dirname "$0")/../docs"
+  cd "$docs_dir" || (echo "cd the workdir $(pkg_dir) fail" && exit)
+  pwd
+}
+
+function build_doc() {
+  cd_docs_dir
+  make html
+}
+
+function publish_preview_doc() {
+  build_doc
+  public_doc preview/doc/html
+  echo "please visit link https://pai-sdk.oss-cn-shanghai.aliyuncs.com/pai/preview/doc/html/index.html to view the documents."
+}
+
+function public_production_doc() {
+  build_doc
+  public_doc doc/html
+  echo "please visit link https://pai-sdk.oss-cn-shanghai.aliyuncs.com/pai/doc/html/index.html to view the documents."
+}
+#
+function public_doc() {
+  if [ -z "$1" ]; then
+    echo "target_path not exists" && exit 1
+  fi
+  echo "target_path is $1"
+
+  ossutilmac64 cp build/html oss://pai-sdk/pai/"$1" --recursive -f
+}
+
+release_type=${1:-preview}
+
+if [ $release_type == "preview" ]; then
+  publish_preview_doc
+elif [ $release_type == "production" ]; then
+  public_production_doc
+else
+  echo "unknown release_type: $release_type" && exit 1
+fi
