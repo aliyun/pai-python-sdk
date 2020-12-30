abe3d7a91d9a3ea11ac4aa2daca1768c78bc7e56
liangquan.lq <lq262469@alibaba-inc.com>
2020-12-30 17:36:21 +0800

    [to #31725045] fix package __version__ read
    Signed-off-by: liangquan.lq <lq262469@alibaba-inc.com>

--- pai/__init__.py
+++ pai/__init__.py
@@ -4,6 +4,6 @@ import os
 version_file = os.path.join(os.path.dirname(__file__), "VERSION")
 if os.path.exists(version_file):
     with open(version_file, "r") as f:
-        __version__ = f.read()
+        __version__ = f.read().strip()
 else:
     __version__ = None
